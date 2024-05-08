e3d1ef94ec6a4382e7da8b00dcc0266726b51993
DilanAshlyn2000 <165892588+DilanAshlyn2000@users.noreply.github.com>
2024-05-08 18:00:08 +0530

    CIA marks, Fees Details and Feedback added

--- collegeManagement_studentDetails.sql
+++ collegeManagement_studentDetails.sql
@@ -1,2 +1,4 @@
 SELECT * FROM collegedetails.studentdetails;
 ALTER TABLE collegedetails.studentdetails ADD feedback VARCHAR(500);
+ALTER TABLE collegedetails.studentdetails ADD feesDetails VARCHAR(7);
+ALTER TABLE collegedetails.studentdetails ADD CIA float;
